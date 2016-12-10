30eeb696be3b39f61e92c1358e0fdbfec2a73f16
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2016-12-10 16:50:46 +0100

    Finished index.php controller
    Finished index.php controller which renders the portfolio.php view

--- pset7/public/index.php
+++ pset7/public/index.php
@@ -5,47 +5,62 @@
 
     $rows = CS50::query("SELECT symbol, shares FROM portfolios WHERE user_id = ?", $_SESSION["id"]);
     
-            print_r($rows);
-            print_r("</br></br>");        
-            print_r($rows[0]);
-            print_r("</br>");  
-            print_r($rows[1]);
-            print_r("</br>");  
-            print_r($rows[2]);
-            print_r("</br></br>"); 
+            // print_r($rows);
+            // print_r("</br></br>");        
+            // print_r($rows[0]);
+            // print_r("</br>");  
+            // print_r($rows[1]);
+            // print_r("</br>");  
+            // print_r($rows[2]);
+            // print_r("</br></br>"); 
                     // print_r("<p>\$rows var: |".$rows[$keys[0]]."| |".$rows[$keys[1]]."|</p>");
     $positions = [];
-            print_r("positions array: ");
-            print_r($positions);
-            print_r("</br></br>");
+    $sum_total = 0;
+    $balance = [];
+            // print_r("positions array: ");
+            // print_r($positions);
+            // print_r("</br></br>");
             // print_r("<p>\$rows var: |".$positions."|</p>");
     foreach ($rows as $row)
     {
         $stock = lookup($row["symbol"]);
-                print_r("stock array: ");
-                print_r($stock);
-                print_r("</br>");
+                // print_r("stock array: ");
+                // print_r($stock);
+                // print_r("</br>");
                 // print_r("</br></br>");
         if ($stock !== false)
         {
+            $total = $row["shares"] * $stock["price"];
+            
             $positions[] = [
                 "name" => $stock["name"],
                 "price" => $stock["price"],
                 "shares" => $row["shares"],
-                "symbol" => $row["symbol"]
+                "symbol" => $row["symbol"],
+                "total" => $total
             ];
-            print_r($stock["name"]);
-            print_r("</br>");
-            print_r($stock["price"]);
-            print_r("</br>");
-            print_r($row["shares"]);
-            print_r("</br>");
-            print_r($row["symbol"]);
-            print_r("</br></br>");
+            // print_r($stock["name"]);
+            // print_r("</br>");
+            // print_r($stock["price"]);
+            // print_r("</br>");
+            // print_r($row["shares"]);
+            // print_r("</br>");
+            // print_r($row["symbol"]);
+            // print_r("</br></br>");
+            // print_r($total);
+            // print_r("</br></br>");
+            $sum_total = $sum_total + $total;
+            // print_r("this is total sum: ");
+            // print_r($sum_total);
+            // print_r("</br></br>");
         }
     }
     
+    
+    $balance = ["balance" => $sum_total];
+    // print_r($balance["balance"]);
+    //         print_r("</br></br>");
     // render portfolio
-    render("portfolio.php", ["positions" => $positions, "title" => "Portfolio"]);
+    render("portfolio.php", ["positions" => $positions, "balance" => $balance, "title" => "Portfolio"]);
 
 ?>
