cac9ef56a352468821297bae4301652062ceb3bd
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2016-12-10 20:35:18 +0100

    add views/sell_form.php and views/sell.php
    Add views/sell_form.php and views/sell.php which are rendered by public/sell.php controller

---
+++ pset7/view/sell (1).php
@@ -0,0 +1,5 @@
+<p class="bal">
+    <?=
+        ("You sold ".$name." stocks for: ".$cash_to_update);
+    ?>
+</p>
---
+++ pset7/view/sell_form.php
@@ -0,0 +1,5 @@
+<form action="sell.php" method="post">
+    <h2>Sell</h2>
+    <input type="text" name="symbol" placeholder="Stock symbol" />
+    <input type="submit" value="sell" />
+</form>
