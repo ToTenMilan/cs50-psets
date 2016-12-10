f650d25c22b3c1b08e349333467c13615e66c929
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2016-12-10 16:51:50 +0100

    Finished portfolio.php view
    Finished portfolio.php view rendered by index.php controller

--- pset7/view/portfolio.php
+++ pset7/view/portfolio.php
@@ -1,14 +1,25 @@
-<table>
+<table class="table">
     <div>
-        <?php foreach ($positions as $position): ?>
-    
-        <tr class="table table-striped">
-            <td><?= $position["name"] ?></td>
-            <td><?= $position["symbol"] ?></td>
-            <td><?= $position["shares"] ?></td>
-            <td><?= $position["price"] ?></td>
+        <tr>
+            <td class="cell_top">Name</td>
+            <td class="cell_top">Symbol</td>
+            <td class="cell_top">Shares</td>
+            <td class="cell_top">Price</td>
+            <td class="cell_top">Total</td>
         </tr>
+        <?php foreach ($positions as $position): ?>
+        
+        <tr class="table">
+            <td class="cell"><?= $position["name"] ?></td>
+            <td class="cell"><?= $position["symbol"] ?></td>
+            <td class="cell"><?= $position["shares"] ?></td>
+            <td class="cell">$<?= $position["price"] ?></td>
+            <td class="cell">$<?= $position["total"] ?></td>
+        </tr>
+        
     
         <?php endforeach ?>
     </div>
 </table>
+<p id="bal">Current balance</p>
+<p id="bal_sum">$<?= $balance["balance"] ?></p>
