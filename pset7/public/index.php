2ad6ff5ed167ab6f2b56cf62a0ff9a83784f1686
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2016-12-12 17:58:27 +0100

    Refactor the variables
    Refactor the variables

--- pset7/public/index.php
+++ pset7/public/index.php
@@ -1,8 +1,9 @@
 <?php
+/* homepage of cs50 finance */
 
     // configuration
     require("../includes/config.php");
-
+    // save in $rows symbol and shares columns of current user
     $rows = CS50::query("SELECT symbol, shares FROM portfolios WHERE user_id = ?", $_SESSION["id"]);
     
             // print_r($rows);
@@ -13,21 +14,24 @@
             // print_r("</br>");  
             // print_r($rows[2]);
             // print_r("</br></br>"); 
-                    // print_r("<p>\$rows var: |".$rows[$keys[0]]."| |".$rows[$keys[1]]."|</p>");
+                    
     $positions = [];
-    $sum_total = 0;
-    $balance = [];
+    // $sum_total = 0;
+    $balance = 0;
+    
             // print_r("positions array: ");
             // print_r($positions);
             // print_r("</br></br>");
-            // print_r("<p>\$rows var: |".$positions."|</p>");
+            
     foreach ($rows as $row)
     {
         $stock = lookup($row["symbol"]);
+        
                 // print_r("stock array: ");
                 // print_r($stock);
                 // print_r("</br>");
                 // print_r("</br></br>");
+                
         if ($stock !== false)
         {
             $total = $row["shares"] * $stock["price"];
@@ -39,33 +43,41 @@
                 "symbol" => $row["symbol"],
                 "total" => $total
             ];
-            // print_r($stock["name"]);
-            // print_r("</br>");
-            // print_r($stock["price"]);
-            // print_r("</br>");
-            // print_r($row["shares"]);
-            // print_r("</br>");
-            // print_r($row["symbol"]);
-            // print_r("</br></br>");
-            // print_r($total);
-            // print_r("</br></br>");
-            $sum_total = $sum_total + $total;
-            // print_r("this is total sum: ");
-            // print_r($sum_total);
-            // print_r("</br></br>");
+            
+                // print_r($stock["name"]);
+                // print_r("</br>");
+                // print_r($stock["price"]);
+                // print_r("</br>");
+                // print_r($row["shares"]);
+                // print_r("</br>");
+                // print_r($row["symbol"]);
+                // print_r("</br></br>");
+                // print_r($total);
+                // print_r("</br></br>");
+                
+            $balance = $balance + $total;
+            
+                // print_r("this is total sum: ");
+                // print_r($sum_total);
+                // print_r("</br></br>");
         }
     }
+    // grab from db current user's cash value  
     $cash = CS50::query("SELECT cash FROM users WHERE id = ?", $_SESSION["id"]);
+    
+        // print_r($cash);
+        // print_r("</br></br>");
         
-        print_r($cash);
-        print_r("</br></br>");
-    $sum_total = $sum_total + $cash[0]["cash"]; 
-        print_r($sum_total);
-        print_r("</br></br>");
+    // update total sum (free cash + value of all stocks)
+    $balance = $balance + $cash[0]["cash"];
+    
+        // print_r($sum_total);
+        // print_r("</br></br>");
+        
     $cash[0]["cash"] = number_format($cash[0]["cash"], $decimals = 2);
-    $balance = ["balance" => $sum_total];
-    // print_r($balance["balance"]);
-    //         print_r("</br></br>");
+    // $balance = ["balance" => $sum_total];
+    $balance = number_format($balance, $decimals = 2);
+    
     // render portfolio
     render("portfolio.php", ["positions" => $positions, "balance" => $balance, "cash" => $cash, "title" => "Portfolio"]);
 
