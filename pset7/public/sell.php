1fdccf524ad1c83eeb9e9f5c0f577ecdc7701bc6
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2016-12-13 16:42:53 +0100

    fix bugs, refactors, documents
    fix bugs, refactors, documents, fully working sell.php controller

--- pset7/public/sell.php
+++ pset7/public/sell.php
@@ -8,9 +8,13 @@
     }
     else if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
+        // save input in variable $input_symbol
         $input_symbol = strtoupper($_POST["symbol"]);
+        // lookup $input_symbol for variable and save it in $yahoo_stock array
         $yahoo_stock = lookup($input_symbol);
+        // save symbol of $yahoo_stock array in $yahoo_symbol
         $yahoo_symbol = $yahoo_stock["symbol"];
+        
         if (empty($input_symbol))
         {
             apologize("Please choose a valid symbol");
@@ -21,26 +25,21 @@
         }
         else
         {
+            // search the database for a symbol given in input
             $db_symbol = CS50::query("SELECT symbol FROM portfolios WHERE user_id = ? AND symbol = ?", $_SESSION["id"], $input_symbol);
-                    // print_r("input symbol: "); print_r($input_symbol);
-                    // print_r("db symbol: "); print_r($db_symbol);
             if ($input_symbol === $db_symbol[0]["symbol"])
             {
-                            print_r("input symbol: "); print_r($input_symbol); print_r("</br></br>");
+                // get from datebase number of shares of $db_symbol
                 $shares_owned = CS50::query("SELECT shares FROM portfolios WHERE user_id = ? AND symbol = ?", $_SESSION["id"], $input_symbol);
+                // calculate the total amount
                 $yahoo_price = $yahoo_stock["price"];
-                            print_r("yahoo price: "); print_r($yahoo_price); print_r("</br></br>");
                 $shares_owned_value = $shares_owned[0]["shares"];
-                            print_r("shares owned value: "); print_r($shares_owned_value); print_r("</br></br>");
                 $total = $shares_owned_value * $yahoo_price;
-                            print_r("total: "); print_r($total); print_r("</br></br>");
+                // delete row with requested symbol from portfolios table
                 CS50::query("DELETE FROM portfolios WHERE user_id = ? AND symbol = ?", $_SESSION["id"], $input_symbol);
-                            print_r("after first db query"); print_r("</br></br>");
+                // update database users with $total_sum + current cash amount
                 CS50::query("UPDATE users SET cash = cash + ? WHERE id = ?", $total , $_SESSION["id"]);
-                            print_r("after second db query"); print_r("</br></br>");
-                            
                 $yahoo_name = $yahoo_stock["name"];
-                            print_r("renders view</br>");
                 render("sell.php", ["total" => $total, "name" => $yahoo_name, "title" => "Stocks sold"]);
             }
             else
@@ -50,7 +49,3 @@
         }
     }
 ?>
-
-            <!--print_r("\$owned_shareS[0][\"symbol\"]: ");-->
-            <!--print_r($owned_shares[0]["symbol"]);-->
-            <!--print_r("</br></br>");  -->
