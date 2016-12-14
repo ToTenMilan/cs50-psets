73e328e8a9da9e74b80d1ce28c998c6857d77624
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2016-12-14 13:33:42 +0100

    account.php view
    account.php view rendered by public/account.php controller

---
+++ pset7/view/account.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Your password has been changed and now check this out";
+?>
+</br></br>
+<iframe width="420" height="315" src="https://www.youtube.com/embed/oHg5SJYRHA0?autoplay=1&iv_load_policy=3" frameborder="0" allowfullscreen></iframe>
+    
