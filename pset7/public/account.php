29c1728ac5dda88e4b9b1ac00cc8e2de99af82ff
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2016-12-14 13:48:29 +0100

    documents & cleans account.php controller
    documents & cleans from print_r's account.php controller

--- pset7/public/account.php
+++ pset7/public/account.php
@@ -1,40 +1,30 @@
 <?php
-
     // configuration
     require("../includes/config.php");
     
-    // if user reached page via GET (as by clicking a link or via redirect)
     if ($_SERVER["REQUEST_METHOD"] == "GET")
     {
-        // else render form
         render("account_form.php", ["title" => "My account"]);
     }
-
-    // else if user reached page via POST (as by submitting a form via POST)
     else if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        // $old_password = password_hash($_POST["old_password"], PASSWORD_BCRYPT);
-                    print_r("old password: "); print_r($_POST["old_password"]); print_r("</br></br>");
+        // save hashed new_password
         $new_password = password_hash($_POST["new_password"], PASSWORD_DEFAULT);
-                    print_r("new password: "); print_r($_POST["new_password"]); print_r("</br></br>");
-        // $new_password_conf = password_hash($_POST["new_password_conf"], PASSWORD_BCRYPT);
-                    print_r("new password conf: "); print_r($_POST["new_password_conf"]); print_r("</br></br>");
+        // check for any blank fields
         if (empty($_POST["old_password"]) || empty($_POST["new_password"]) || empty($_POST["new_password_conf"]))
         {
             apologize("Any field can't be blank");
         }
-        
+        // grab from db current password hash
         $current_hash = CS50::query("SELECT hash FROM users WHERE id = ?", $_SESSION["id"]);
-        $current_hash = $current_hash[0]["hash"];
-                    print_r("current hash: "); print_r($current_hash); print_r("</br></br>");
-        $verified = password_verify($_POST["old_password"], $current_hash);
-                    print_r("verified: "); print_r($verified); print_r("</br></br>");
-        if ($verified)
+        // verify the old password
+        if (password_verify($_POST["old_password"], $current_hash[0]["hash"]))
         {
+            // verify confirmation password against new password
             if (password_verify($_POST["new_password_conf"], $new_password))
             {
+                // update database with new password
                 CS50::query("UPDATE users SET hash = ? WHERE id = ?", $new_password, $_SESSION["id"]);
-                
                 render("account.php", ["title" => "Password changed"]);
             }
             else
@@ -46,11 +36,5 @@
         {
             apologize("Your password is wrong.");
         }
-        
-        
-        
-        
-        
     }
-
 ?>
